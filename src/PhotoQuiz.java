a5a7336105e1f82ae2ec60775b280aca9327a415
11000783 <josephaguilar2022@gmail.com>
2019-01-19 17:27:14 -0800

    PhotoQuiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -26,21 +26,37 @@ public class PhotoQuiz {
 		// 2. create a variable of type "Component" that will hold your image
                Component hold;
 		// 3. use the "createImage()" method below to initialize your Component
-                     
+            hold =    createImage(image);         
 		// 4. add the image to the quiz window
-
+             quizWindow.add(hold);
 		// 5. call the pack() method on the quiz window
-
+                quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+        String awesome =  JOptionPane.showInputDialog("What is in the third inventory slot?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+                if(awesome.equals( "heavy shotgun")) {
+                System.out.println("CORRECT");
+                }
+                else {
+                	System.out.println("INCORRECT");
+                }
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+               quizWindow.remove(hold);
 		// 10. find another image and create it (might take more than one line of code)
-
+                    String image2 = "https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/59/fd/d0/59fdd0a8-9fba-8c94-e403-887a36ae5e11/mzl.bkjzkqwu.png/643x0w.jpg";
+                 Component hold2;
+                 hold2 = createImage(image2);
+                 quizWindow.add(hold2);
+                 quizWindow.pack();
+                 String cool = JOptionPane.showInputDialog("What weapon is in the second inventory slot?");
+                 if(cool.equals("minigun")) {
+                	 System.out.println("CORRECT");
+                 }
+                 else {
+                	 System.out.println("INCORRECT");
+                 }
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
